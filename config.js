7a012070f6219c0c0ba68ced31070919007eb411
Sharky! <jdeurt@gmail.com>
2018-04-18 14:27:19 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,15 @@
 module.exports = {
     ports: {
-        GREEN_LED: 13,
-        RED_LED: 12
+        SHOULDER_SERVO: 0,
+        ELBOW_SERVO: 1,
+        WRIST_SERVO: 2,
+        THUMB_MOTOR: 3,
+        INDEX_FINGER_MOTOR: 4,
+        MIDDLE_FINGER_MOTOR: 5,
+        RING_FINGER_MOTOR: 6,
+        PINKY_FINGER_MOTOR: 7,
+        SUCCESS_LED: 8,
+        WARN_LED: 9,
+        ERROR_LED: 10
     }
 };
