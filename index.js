006e319edad14ea6011c53dc10ffd34f97ed116e
Sharky! <jdeurt@gmail.com>
2018-04-18 14:34:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,27 @@ const five = require("johnny-five");
 const board = new five.Board();
 
 board.on("ready", function() {
-    //define var led as an LED light connected to port 13
-    let led = new five.Led(GREEN_LED);
-    //make the led blink every 500ms
-    led.blink(500);
+    let led = {
+        success: new five.Led(config.ports.SUCCESS_LED),
+        warn: new five.Led(config.ports.WARN_LED),
+        error: new five.Led(config.ports.ERROR_LED)
+    };
+    let arm = {
+        shoulder: new five.Servo(config.ports.SHOULDER_SERVO),
+        elbow: new five.Servo(config.ports.ELBOW_SERVO),
+        wrist: new five.Servo(config.ports.WRIST_SERVO),
+        fingers: {
+            thumb: new five.Servo(config.ports.THUMB_MOTOR),
+            index: new five.Servo(config.ports.INDEX_FINGER_MOTOR),
+            middle: new five.Servo(config.ports.MIDDLE_FINGER_MOTOR),
+            ring: new five.Servo(config.ports.RING_FINGER_MOTOR),
+            pinky: new five.Servo(config.ports.PINKY_FINGER_MOTOR),
+        }
+    };
+    for(let prop in arm.fingers) {
+        arm.fingers[prop].to(0);
+    }
+    arm.fingers.middle.to(180);
 });
+
+
