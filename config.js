65f36d867481d3ca4ec929a974c669576f5c2fff
Sharky! <jdeurt@gmail.com>
2018-04-20 14:04:57 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,13 @@
 module.exports = {
     ports: {
-        SHOULDER_SERVO: 0,
-        ELBOW_SERVO: 1,
+        ELBOW_SERVO: 0,
+        FOREARM_SERVO: 1,
         WRIST_SERVO: 2,
         THUMB_MOTOR: 3,
-        INDEX_FINGER_MOTOR: 4,
-        MIDDLE_FINGER_MOTOR: 5,
-        RING_FINGER_MOTOR: 6,
-        PINKY_FINGER_MOTOR: 7,
-        SUCCESS_LED: 8,
-        WARN_LED: 9,
-        ERROR_LED: 10
+        G1_MOTOR: 4, //G1 is index + middle
+        G2_MOTOR: 5, //G2 is ring + pinky
+        SUCCESS_LED: 6,
+        WARN_LED: 7,
+        ERROR_LED: 8
     }
 };
